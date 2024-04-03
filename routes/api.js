d886e8023376fbee2ffb3769b033e70024ffdfb5
Carolina Alves <carolina_rocha3@hotmail.com>
2024-04-03 02:03:27 +0000

    add mongodb uri

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,12 @@
 'use strict';
 
+var expect = required('chai').expect;
+let mongodb = require('mongodb')
+let mongoose = require('mongoose')
+
 module.exports = function (app) {
+
+  let uri = 'mongodb+srv://carolina:' + process.env.PW + '@freecodecamp.ys83xkg.mongodb.net/FreeCodeCamp?retryWrites=true&w=majority&appName=FreeCodeCamp'
 
   app.route('/api/stock-prices')
     .get(function (req, res){
