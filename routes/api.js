0dd2b75a1bb766dd2def7c063b89912ac1b066f1
Carolina Alves <carolina_rocha3@hotmail.com>
2024-04-11 22:04:17 +0000

    mongodb connect + security policies

--- routes/api.js
+++ routes/api.js
@@ -8,6 +8,16 @@ module.exports = function (app) {
 
   let uri = 'mongodb+srv://carolina:' + process.env.PW + '@freecodecamp.ys83xkg.mongodb.net/FreeCodeCamp?retryWrites=true&w=majority&appName=FreeCodeCamp'
 
+  mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+
+  let stockSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    likes: {types: Number, default: 0},
+    ips: [String]
+  })
+
+  let Stock = mongoose.model('Stock', stockSchema)
+
   app.route('/api/stock-prices')
     .get(function (req, res){
       
